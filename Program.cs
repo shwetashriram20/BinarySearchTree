6d3dca10d52e1f32e382bc180a1a2fc26f8df9eb
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-13 19:26:23 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using BinarySearchTrees;
+
+namespace BinarySearchTrees
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Binarytree bn = new Binarytree();
+            bn.Add(56);
+            bn.Add(30);
+            bn.Add(70);
+
+            Console.WriteLine();
+            Console.WriteLine("the preorder is: ");
+            bn.TravesrePreorder(bn.root);
+            Console.WriteLine();
+            Console.WriteLine("the postorder is: ");
+            bn.TravesrePostorder(bn.root);
+            Console.WriteLine();
+            Console.WriteLine("the Inorder is: ");
+            bn.TravesreInorder(bn.root);
+            Console.WriteLine();
+
+        }
+    }
+}
+
