24eee6308838a9f88ab99be6cefa8527dae0855f
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-13 19:31:02 +0530

    Add files via upload

---
+++ Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTrees
+{
+    public class Node
+    {
+        public Node leftnode { get; set; }
+        public Node rightnode { get; set; }
+        public int data { get; set; }
+
+    }
+}
+
+
+
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using BinarySearchTrees;
+
+namespace BinarySearchTrees
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Binarytree bn = new Binarytree();
+            bn.Add(56);
+            bn.Add(30);
+            bn.Add(70);
+            bn.Add(22);
+            bn.Add(40);
+            bn.Add(3);
+            bn.Add(11);
+            bn.Add(16);
+            bn.Add(60);
+            bn.Add(65);
+            bn.Add(63);
+            bn.Add(67);
+            bn.Add(95);
+            bn.Add(70);
+
+            Console.WriteLine();
+            Console.WriteLine("the preorder is: ");
+            bn.TravesrePreorder(bn.root);
+            Console.WriteLine();
+            Console.WriteLine("the postorder is: ");
+            bn.TravesrePostorder(bn.root);
+            Console.WriteLine();
+            Console.WriteLine("the Inorder is: ");
+            bn.TravesreInorder(bn.root);
+            Console.WriteLine();
+
+        }
+    }
+}
+
+
+
